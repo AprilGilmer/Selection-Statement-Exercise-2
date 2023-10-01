5563542660bf1f2a6a2dfef8ed94245e0ffcaf9a
April Gilmer <aprilgilmer1@gmail.com>
2023-10-01 16:11:47 -0500

    Selection Statements Exercise TWO completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What are your favorite subjects out of these choices: Math, Science, English, PE, or History?");
+            string schoolSubject = Console.ReadLine().ToLower();
+            switch (schoolSubject)
+            {
+                case "math":
+                    Console.WriteLine("Yay, You like Math!");
+                    break;
+                case "science":
+                    Console.WriteLine("Awesome, I love science too!");
+                    break;
+                case "english":
+                    Console.WriteLine("Really english...I guess I'll let it slide.");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE...Is that really a subject?"); 
+                    break;
+                case "history":
+                    Console.WriteLine("Cool. You must like hanging out with ghost and stuff.");
+                    break;
+                default:
+                    Console.WriteLine("Hmmm..Looks like you didn't read the directions correctly. Please read the directions thoroughly this time around.");
+                    break;
+            }
         }
     }
 }
